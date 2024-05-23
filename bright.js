223b9af7fb89eb5e9a81f025636b5c371070c38c
BRIGHT OMOLAYO <molayobright@gmail.com>
2024-05-23 14:46:54 +0100

    added about page

--- bright.js
+++ bright.js
@@ -1,21 +1,20 @@
 // eslint-disable-next-line no-unused-vars
-// function dropdown () {
-//   const kebabContent = document.getElementById('linksAndSwitchIcon')
+function dropdown () {
+  const kebabContent = document.getElementById('dropdown')
 
+  if (kebabContent.style.display === 'none') {
+    kebabContent.style.display = 'block'
+  } else {
+    kebabContent.style.display = 'none'
+  }
+}
+
+// const kebabIcon = document.getElementById('kebabIcon')
+// kebabIcon.addEventListener('click', function () {
+//   const kebabContent = document.getElementById('linksAndSwitchIcon')
 //   if (kebabContent.style.display === 'none') {
 //     kebabContent.style.display = 'block'
 //   } else {
 //     kebabContent.style.display = 'none'
 //   }
-// }
-
-const kebabIcon = document.getElementById('kebabIcon')
-kebabIcon.addEventListener('click', function () {
-  const kebabContent = document.getElementById('linksAndSwitchIcon')
-
-  if (kebabContent.style.display === 'none') {
-    kebabContent.style.display = 'inline-block'
-  } else {
-    kebabContent.style.display = 'none'
-  }
-})
+// })
