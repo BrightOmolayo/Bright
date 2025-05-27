612475e63b12b9f1d27c5a4f9749021a49df9012
BRIGHT OMOLAYO <molayobright@gmail.com>
2025-05-27 06:18:15 +0100

    Redesigned and redeveloped website

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 // eslint-disable-next-line no-unused-vars
+function toggleSwitch () {
+  document.body.classList.toggle('switch')
+}
+// eslint-disable-next-line no-unused-vars
 function dropdown () {
   const container = document.querySelector('.dropdown')
-  const content = document.querySelector('.hidden')
+  const content = document.querySelector('.nav-content-for-mobilenav-links-and-switch-button')
   container.classList.toggle('active')
   content.classList.toggle('active')
   if (content.style.display === 'none') {
